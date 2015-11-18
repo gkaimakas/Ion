d954b6daad52bc9bcd5465b04290864d623e9a62
George Kaimakas <gkaimakas@gmail.com>
2015-11-18 12:29:23 +0200

    Initial commit

---
+++ Example/Ion/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Ion
+//
+//  Created by George Kaimakas on 11/18/2015.
+//  Copyright (c) 2015 George Kaimakas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
